2bb91228e4ff6b684bc620a436b9acd8f78f9f0c
Dan Hughes <danhughes.dh+github@gmail.com>
2017-02-04 19:03:14 +0000

    Add skeleton functionality for instantiating IO and mappers/reducers

--- src/com/rv007602/acmrlib/Controller.java
+++ src/com/rv007602/acmrlib/Controller.java
@@ -8,4 +8,28 @@ import java.util.HashMap;
 
 public class Controller {
 
+	private Class<? extends Mappable> mapperClass;
+	private Class<? extends Reducible> reducerClass;
+	private BufferedReader input;
+	private BufferedWriter output;
+	private HashMap<String, ArrayList<String>> mapResults = new HashMap<>();
+	private HashMap<String, String> reduceResults = new HashMap<>();
+
+	public void setInput(BufferedReader input) {
+		this.input = input;
+	}
+
+	public void setMapper(Class<? extends Mappable> mapperClass) {
+		this.mapperClass = mapperClass;
+	}
+
+	public void setReducer(Class<? extends Reducible> reducerClass) {
+		this.reducerClass = reducerClass;
+	}
+
+
+	public void setOutput(BufferedWriter output) {
+		this.output = output;
+	}
+
 }
