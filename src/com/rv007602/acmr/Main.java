006c7769e2e7ab7dcca1aab9be7b367d451fcafd
Dan Hughes <danhughes.dh+github@gmail.com>
2017-02-04 18:35:15 +0000

    Add basic outline API from the user's point of view

---
+++ src/com/rv007602/acmr/Main.java
@@ -0,0 +1,26 @@
+package com.rv007602.acmr;
+
+import com.rv007602.acmrlib.Controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Controller controller = new Controller();
+
+		controller.setMapper(Mapper.class);
+		controller.setReducer(Reducer.class);
+
+		BufferedReader input = new BufferedReader(new FileReader("data.txt"));
+		BufferedWriter output = new BufferedWriter(new FileWriter("output.txt"));
+		controller.setInput(input);
+		controller.setOutput(output);
+
+		controller.run();
+	}
+}
+
