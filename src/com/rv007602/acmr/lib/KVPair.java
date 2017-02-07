60aca8c12b609a7e61283fe48b25ef57e3c910e2
Dan Hughes <danhughes.dh+github@gmail.com>
2017-02-07 15:04:17 +0000

    Add JavaDocs to KVPair

--- src/com/rv007602/acmr/lib/KVPair.java
+++ src/com/rv007602/acmr/lib/KVPair.java
@@ -4,6 +4,11 @@ public class KVPair {
 	private final String key;
 	private final String value;
 
+	/**
+	 * Creates a new immutable KVPair.
+	 * @param key The key.
+	 * @param value The value.
+	 */
 	public KVPair(String key, String value) {
 		this.key = key;
 		this.value = value;
@@ -17,6 +22,10 @@ public class KVPair {
 		return this.value;
 	}
 
+	/**
+	 * Converts the KVPair into a CSV-style string.
+	 * @return String representation.
+	 */
 	public String toString() {
 		return this.key + "," + this.value;
 	}
